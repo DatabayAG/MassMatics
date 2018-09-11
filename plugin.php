8e947ca2f52bca09874d53844a33453dbe1290b1
mjansen <mjansen@databay.de>
2018-09-11 10:22:02 +0200

    5.3.x compatibility

--- plugin.php
+++ plugin.php
@@ -4,15 +4,13 @@
 $id = "massmatics";
 
 // code version; must be changed for all code changes
-$version = "0.0.5";
+$version = "1.0.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.0.0";
-$ilias_max_version = "5.2.999";
+$ilias_max_version = "5.3.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Guido Vollbach";
-$responsible_mail = "gvollbach@databay.de";
-
-?>
+$responsible = "Guido Vollbach / Michael Jansen";
+$responsible_mail = "gvollbach@databay.de / mjansen@databay.de";
